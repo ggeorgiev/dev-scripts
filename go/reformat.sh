eeb8fe62c907cc7f70b04282ecae562061256ed6
George Georgiev <george.georgiev.sf@gmail.com>
2016-11-19 18:04:41 -0800

    run clang-format for proto files (if exists)

--- go/reformat.sh
+++ go/reformat.sh
@@ -3,3 +3,9 @@
 go get golang.org/x/tools/cmd/goimports
 find . -name "*.go" | xargs gofmt -w -s
 find . -name "*.go" | xargs goimports -w
+
+if [ ! -z "`which clang-format`" ]
+then
+    find . -name "*.proto" | xargs clang-format -i
+fi
+
