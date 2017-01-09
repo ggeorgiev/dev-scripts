bbba68ffa592ee8f83de6ce76fd0d1835ece07e9
George Georgiev <george.georgiev.sf@gmail.com>
2017-01-08 19:39:11 -0800

    Add removing the empty lines from import sections

--- go/reformat.sh
+++ go/reformat.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#go imports seems to do not touch empty lines in the import section
+find . \( -type d -name vendor -prune -a -type f \) -o -type f -name "*.go" | xargs -L 1 sed -i '' '/^import (/,/^[[:space:]]*)/ { /^[[:space:]]*$/ d; }'
+
 go get golang.org/x/tools/cmd/goimports
 find . \( -type d -name vendor -prune -a -type f \) -o -type f -name "*.go" | xargs gofmt -w -s
 find . \( -type d -name vendor -prune -a -type f \) -o -type f -name "*.go" | xargs goimports -w
