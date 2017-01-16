baeaf99b7ada95a0d80680b95d3ccf3c6d8936cc
George Georgiev <george.georgiev.sf@gmail.com>
2017-01-15 17:13:00 -0800

    Change the clean script to get an argument

--- go/clean_all_but_me.sh
+++ go/clean_all_but_me.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-find $GOPATH/src -path `git rev-parse --show-toplevel` -prune -o -type f -print | xargs -L 1 rm
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 DIRECTORY" >&2
+  exit 1
+fi
+
+REPO=$1
+find $GOPATH/src -path $REPO -prune -o \( -type f -o -type l \) -print | xargs -I{} rm "{}"
 
 while [ `find $GOPATH -type d -empty | wc -l` != 0 ]
 do
