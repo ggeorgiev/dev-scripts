ffb7012cdb4be38822f3eb855016c24a4364721e
George Georgiev <george.georgiev.sf@gmail.com>
2017-12-24 17:13:53 -0800

    Add script that continuosly rebuilts a branch

--- git/alias.sh
+++ git/alias.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pushd `dirname ${BASH_ARGV[0]}`/../.. > /dev/null && BASEDIR=$(pwd -L) && popd > /dev/null
 
+alias gcam='git commit -am'
+alias gsh='git show'
 alias gbr='git branch'
 alias gco='git checkout'
 alias gcom='git checkout master'
