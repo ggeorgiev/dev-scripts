b36dca3c9b955568ca1ce5dc7526b6b240a4e50f
George Georgiev <george.georgiev.sf@gmail.com>
2018-01-20 10:11:41 -0800

    Print all authors

--- git/fame.sh
+++ git/fame.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 pattern=
+order="sort -n"
 exclude="^$"
 substitutes="s/[(]//"
 
 while [ $# -gt 0 ]
 do
   case "$1" in
-    -p) echo $2; pattern="$2"; shift;;
+    -p) pattern="$2"; shift;;
     -s) substitutes="$substitutes;s/$2/"; shift;;
+    -a) order="sort -k 2";;
     -*)
        echo >&2 \
          "usage: $0 [-p pattern]"
@@ -39,8 +41,10 @@ case "$pattern" in
                          *) ;;
 esac
 
-#echo run fame with pattern: $pattern and exclude: $exclude
+echo include: "$pattern" and exclude: "$exclude" order: $order
 
-git ls-files | grep -e "$pattern" | grep -ve "$exclude" \
-  | xargs -L 1 git annotate --minimal -M -C -w -e -s | awk ' { print $2 } ' | sed "$substitutes" \
-  | sort | uniq -c | sort -n
+{
+    (git log --format='<%aE>' | sort -u);
+    (git ls-files | grep -e "$pattern" | grep -ve "$exclude" \
+        | xargs -L 1 git annotate --minimal -M -C -w -e -s | awk ' { print $2 } ' | sed "$substitutes")
+} | sort | uniq -c | $order | awk ' { print $1-1" "$2 } '
